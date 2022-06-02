ea87c0739fd45ac0ee0050a1b49d58b497938921
Parkjju <rudwns3927@gmail.com>
2022-06-02 22:41:29 +0900

    [fix] index.tsx에 theme 프로퍼티 추가

--- todo/src/index.tsx
+++ todo/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { RecoilRoot } from 'recoil';
 import App from './App';
+import { darkTheme } from './theme';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -10,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <RecoilRoot>
-            <ThemeProvider>
+            <ThemeProvider theme={darkTheme}>
                 <App />
             </ThemeProvider>
         </RecoilRoot>
