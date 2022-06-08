a8d6c6121d4603c6acd44cf63d1e425a30df2954
Parkjju <rudwns3927@gmail.com>
2022-06-08 21:31:04 +0900

    [Feature] derived state, Recoil Selector 활용

--- todo/src/components/Todo.tsx
+++ todo/src/components/Todo.tsx
@@ -1,13 +1,31 @@
-import { ITodo } from '../atoms';
+import React from 'react';
+import { useSetRecoilState } from 'recoil';
+import { ITodo, toDoState } from '../atoms';
 
 function Todo({ text, id, category }: ITodo) {
+    const setToDos = useSetRecoilState(toDoState);
+    const onClick = (newCategory: ITodo['category']) => {
+        setToDos((oldToDos) => {
+            const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+            const newToDo = { text, id, category: newCategory };
+            const newToDos = [...oldToDos];
+            newToDos.splice(targetIndex, 1, newToDo);
+
+            return newToDos;
+        });
+    };
     return (
-        <li key={id}>
+        <li>
             <span>{text}</span>
-            <button>Done</button>
-            <button>Doing</button>
-            <button>To Do</button>
-            {category}
+            {category !== 'DOING' && (
+                <button onClick={() => onClick('DOING')}>Doing</button>
+            )}
+            {category !== 'TO_DO' && (
+                <button onClick={() => onClick('TO_DO')}>To Do</button>
+            )}
+            {category !== 'DONE' && (
+                <button onClick={() => onClick('DONE')}>DONE</button>
+            )}
         </li>
     );
 }
--- todo/src/components/TodoList.tsx
+++ todo/src/components/TodoList.tsx
@@ -1,21 +1,41 @@
 import { useRecoilValue } from 'recoil';
 import CreateTodo from './CreateTodo';
-import { toDoState } from '../atoms';
+import { toDoSelector } from '../atoms';
 import Todo from './Todo';
 
 function TodoList() {
-    const toDos = useRecoilValue(toDoState);
+    const [toDo, doing, done] = useRecoilValue(toDoSelector);
 
     return (
         <div>
             <h1>To Dos</h1>
             <hr />
             <CreateTodo />
+            <h2>To Do</h2>
+
             <ul>
-                {toDos.map((todo) => (
+                {toDo.map((todo) => (
                     <Todo key={todo.id} {...todo} />
                 ))}
             </ul>
+            <hr />
+            <h2>Doing</h2>
+
+            <ul>
+                {doing.map((todo) => (
+                    <Todo key={todo.id} {...todo} />
+                ))}
+            </ul>
+            <hr />
+
+            <h2>Done</h2>
+
+            <ul>
+                {done.map((todo) => (
+                    <Todo key={todo.id} {...todo} />
+                ))}
+            </ul>
+            <hr />
         </div>
     );
 }
